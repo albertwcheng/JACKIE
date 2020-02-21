cd3ffb65683a52b4112baca022a17c8d2de0237f
Albert Cheng <albertwcheng@gmail.com>
2020-02-21 13:32:27 -0500

    Fri Feb 21 13:32:27 EST 2020

--- example.sh
+++ example.sh
@@ -1,21 +1,34 @@
 
-
-export PATH=/hpcdata/wcheng/test/bin:${PATH}
+#remember to add JACKIE to your path, e.g.,
+#export PATH=/hpcdata/wcheng/test/bin:${PATH}
+#
+#
 
 genome=hg38
-genomeFasta=/hpcdata/wcheng/genomes/$genome/$genome.nr.fa
-pamFold=/hpcdata/wcheng/genomes/$genome/pamFold/
+pathToGenome=/hpcdata/wcheng/genomes/$genome
+genomeFasta=$pathToGenome/$genome.nr.fa
+pamFold=$pathToGenome/pamFold/
 
-mkdir $pamFold
+#download hg38
+#cd $pathToGenome
+#wget --timestamping 'ftp://hgdownload.cse.ucsc.edu/goldenPath/mm10/chromosomes/*'
+#gunzip *.gz
+#mkdir random
+#mv *random*.fa random/
+#mv chrUn*.fa random/
+#mkdir nr
+#mv *.fa nr
+#cat nr/*.fa > $genome.nr.fa
+#mkdir $pamFold
 
 
 for N in A C G T; do
-echo "JACKIE -b2 $genomeFasta $pamFold .bin 6 $pamFold/$N.ref.txt $N n" | qsub -l walltime=48:00:00
+echo "date; JACKIE -b2 $genomeFasta $pamFold .bin 6 $pamFold/$N.ref.txt $N n; date" | qsub -l walltime=48:00:00
 done
 
 
 for prefix in AA AC AT AG CA CC CT CG TA TC TT TG GA GC GT GG; do
-echo "outbedForPrefixJob.sh $pamFold $prefix 1 0" | qsub -l walltime=24:00:00 -e `pwd`/$prefix.stderr.txt -o `pwd`/$prefix.stdout.txt	
+echo "date; outbedForPrefixJob.sh $pamFold $prefix 1 0; date" | qsub -l walltime=24:00:00 -e `pwd`/$prefix.stderr.txt -o `pwd`/$prefix.stdout.txt	
 done
 
 cat $pamFold/*.bed > $pamFold/${genome}PAM.BED
@@ -29,8 +42,3 @@ echo "removeIllegalBlockEntries.py $pamFold/${genome}PAM.sameChr.tx.sorted.bed $
 
 
 
-
-
-#echo "chainExonBedsToTranscriptBed.py `pwd`/${genome}PAM.BED 0 > `pwd`/${genome}PAM.sameChr.tx.bed" | qsub -l walltime=24:00:00
-#echo "sort -k1,1 -k2,2n `pwd`/${genome}PAM.sameChr.tx.bed > `pwd`/${genome}PAM.sameChr.tx.sorted.bed" | qsub -l walltime=24:00:00
-#echo "removeIllegalBlockEntries.py `pwd`/${genome}PAM.sameChr.tx.sorted.bed `pwd`/${genome}PAM.sameChr.tx.sorted.legal.bed `pwd`/${genome}PAM.sameChr.tx.sorted.illegal.bed" | qsub -l walltime=24:00:00

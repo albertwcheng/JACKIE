58dcba49016a69bc1594fe3f8555c38026e316d0
Albert Cheng <albertwcheng@gmail.com>
2020-02-21 13:41:05 -0500

    Fri Feb 21 13:41:05 EST 2020

--- example.sh
+++ example.sh
@@ -1,42 +1,50 @@
 
-#remember to add JACKIE to your path, e.g.,
+#remember to add JACKIE to your path in ~/.bashrc file
+#
+#e.g.,
 #export PATH=/hpcdata/wcheng/test/bin:${PATH}
 #
 #
 
-genome=hg38
-pathToGenome=/hpcdata/wcheng/genomes/$genome
+genome=<fill in your genome> #e.g., hg38
+genomesRoot=<fill in your root path> #e.g.,/hpcdata/wcheng/genomes
+pathToGenome=$genomesRoot/$genome
 genomeFasta=$pathToGenome/$genome.nr.fa
 pamFold=$pathToGenome/pamFold/
 
-#download hg38
-#cd $pathToGenome
-#wget --timestamping 'ftp://hgdownload.cse.ucsc.edu/goldenPath/mm10/chromosomes/*'
-#gunzip *.gz
-#mkdir random
-#mv *random*.fa random/
-#mv chrUn*.fa random/
-#mkdir nr
-#mv *.fa nr
-#cat nr/*.fa > $genome.nr.fa
-#mkdir $pamFold
+#download genome fasta files and produce a merged files for non-random chromosomes
+cd $pathToGenome
+wget --timestamping "ftp://hgdownload.cse.ucsc.edu/goldenPath/$genome/chromosomes/*"
+gunzip *.gz
+mkdir random
+mv *random*.fa random/
+mv chrUn*.fa random/
+mkdir nr
+mv *.fa nr
+cat nr/*.fa > $genome.nr.fa
+mkdir $pamFold
 
 
+#generate binary represetation of sgRNA binding locations
 for N in A C G T; do
 echo "date; JACKIE -b2 $genomeFasta $pamFold .bin 6 $pamFold/$N.ref.txt $N n; date" | qsub -l walltime=48:00:00
 done
 
-
+#output bed file from binary files.
 for prefix in AA AC AT AG CA CC CT CG TA TC TT TG GA GC GT GG; do
 echo "date; outbedForPrefixJob.sh $pamFold $prefix 1 0; date" | qsub -l walltime=24:00:00 -e `pwd`/$prefix.stderr.txt -o `pwd`/$prefix.stdout.txt	
 done
 
+#concatenate all bed files into one
 cat $pamFold/*.bed > $pamFold/${genome}PAM.BED
 
+#collapse sgRNA binding locations with same sequecnes into an extended bed format
 echo "chainExonBedsToTranscriptBed.py $pamFold/${genome}PAM.BED 0 > $pamFold/${genome}PAM.sameChr.tx.bed" | qsub -l walltime=24:00:00
 
+#sort extended bed file
 echo "sort -k1,1 -k2,2n $pamFold/${genome}PAM.sameChr.tx.bed > $pamFold/${genome}PAM.sameChr.tx.sorted.bed" | qsub -l walltime=24:00:00
 
+#remove entries with overlapping sgRNA sites.
 echo "removeIllegalBlockEntries.py $pamFold/${genome}PAM.sameChr.tx.sorted.bed $pamFold/${genome}PAM.sameChr.tx.sorted.legal.bed $pamFold/${genome}PAM.sameChr.tx.sorted.illegal.bed" | qsub -l walltime=24:00:00
 
 
